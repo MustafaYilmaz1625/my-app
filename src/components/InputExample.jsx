87a2a1a117fdc99ef40442023eeaae41ce9010d7
Mustafa Yılmaz <105638480+MustafaYilmaz1625@users.noreply.github.com>
2022-06-01 13:57:19 +0300

    Input example..

---
+++ src/components/InputExample.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function InputExample() {
+  const [form, setForm] = useState({ name: "", surname: "" });
+  const onChangeInput = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div>
+      Name <br />
+      <input name="name" value={form.name} onChange={onChangeInput} />
+      <br />
+      <br />
+      Surname <br />
+      <input name="surname" value={form.surname} onChange={onChangeInput} />
+      <br />
+      <br />
+      {form.name} {form.surname}
+    </div>
+  );
+}
+
+export default InputExample;
